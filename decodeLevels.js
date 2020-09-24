9008012b0a471ed698b876674a4f1535f6e8389b
willycoffee <67596846+willycoffee@users.noreply.github.com>
2020-09-24 18:43:55 -0400

    Update decodeLevels.js

--- decodeLevels.js
+++ decodeLevels.js
@@ -2,9 +2,7 @@ const fs = require('fs')
 const Crypto = require('./crypto.js')
 let crypto = new Crypto()
 
-let gdSave = process.env.HOME || process.env.USERPROFILE + "/AppData/Local/GeometryDash/CCLocalLevels.dat"
-
-fs.readFile(gdSave, 'utf8', function (err, saveData) {
+fs.readFile('CCLocalLevels.dat', 'utf8', function (err, saveData) {
     if (err) return console.log("Error! Could not open or find GD save file")
     let decoded = crypto.decode(saveData)
     if (!decoded) return
